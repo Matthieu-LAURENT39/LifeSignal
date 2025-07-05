52ee936d60261a067e8c40200753f03336d0d992
Mathis² <mathisfr45@gmail.com>
2025-07-05 11:08:53 +0200

    Setup smart contract env for Oasis

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,33 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@oasisprotocol/sapphire-hardhat";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env
+dotenv.config();
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : {
+  mnemonic: "test test test test test test test test test test test junk",
+  path: "m/44'/60'/0'/0",
+  initialIndex: 0,
+  count: 20,
+  passphrase: "",
+};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sapphire: {
+      url: "https://sapphire.oasis.io",
+      chainId: 0x5afe,
+      accounts,
+    },
+    "sapphire-testnet": {
+      url: "https://testnet.sapphire.oasis.io",
+      accounts,
+      chainId: 0x5aff,
+    }
+  }
 };
 
 export default config;
